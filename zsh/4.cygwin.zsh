704bc292631b137e2e59cbfafe0951f52835c5c4
Matthew Scharley <mscharley@joyer.com.au>
2011-06-30 13:55:47 +1000

    Added some extra stuff for zsh in cygwin.

---
+++ zsh/4.cygwin.zsh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+# Check we're running under cygwin
+if [[ $(uname -o) != 'Cygwin' ]]; then
+  return
+fi
+
+# Kill Windows paths by default. If you need these, add them to your
+# zshrc.local manually.
+NEWPATH=()
+for p in ${(s.:.)PATH}; do
+  if [[ $p[0,10] != "/cygdrive/" ]]; then
+    NEWPATH+=($p)
+  fi
+done
+PATH=${(j.:.)NEWPATH}
+unset NEWPATH
+
