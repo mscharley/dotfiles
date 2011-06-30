1d31c4d108bb21b76f96ba883e6319b9da7217ab
Matthew Scharley <matt.scharley@gmail.com>
2011-06-30 19:24:28 +1000

    Switched up some of the colors of the prompt as the brown is icky.

--- zsh/1.prompt.zsh
+++ zsh/1.prompt.zsh
@@ -5,20 +5,20 @@ autoload colors zsh/terminfo
 if [[ "$terminfo[colors]" -ge 8 ]]; then
   colors
 fi
-for color in RED GREEN YELLOW BLUE MAGENTA CYAN WHITE; do
+for color in RED GREEN YELLOW BLUE MAGENTA CYAN WHITE BLACK; do
   eval PR_LIGHT_$color='%{$terminfo[bold]$fg[${(L)color}]%}'
   eval PR_$color='%{$fg[${(L)color}]%}'
 done
 PR_NO_COLOR="%{$terminfo[sgr0]%}"
 
 parse_git_branch() {
-  git name-rev HEAD 2> /dev/null | sed "s#HEAD \(.*\)# [$PR_LIGHT_RED\1$PR_NO_COLOR]#"
+  git name-rev HEAD 2> /dev/null | sed "s#HEAD \(.*\)# $PR_LIGHT_BLACK""[$PR_NO_COLOR$PR_RED\1$PR_LIGHT_BLACK]$PR_NO_COLOR#"
 }
 
 # Enable substitutions in the prompt
 setopt PROMPT_SUBST
 
 # Actual prompt variables
-PROMPT='[$PR_YELLOW%n@%m $PR_GREEN%2c$PR_NO_COLOR]$(parse_git_branch)$PR_NO_COLOR%(!.#.$) '
-RPROMPT='[$PR_YELLOW%?$PR_NO_COLOR]';
+PROMPT="$PR_LIGHT_BLACK"'[$PR_NO_COLOR$PR_RED%n@%m $PR_GREEN%2c$PR_LIGHT_BLACK]$(parse_git_branch)$PR_NO_COLOR%(!.#.$) '
+RPROMPT="$PR_LIGHT_BLACK"'[%?]'"$PR_NO_COLOR";
 
