024d18fa815a1bbda1fe7b119c65e8ecafe7b3f1
Matthew Scharley <matt.scharley@gmail.com>
2012-06-15 18:39:10 +1000

    Cleaned up the PR_COLOR variables

--- zsh/1.prompt.zsh
+++ zsh/1.prompt.zsh
@@ -5,16 +5,16 @@ autoload colors zsh/terminfo
 if [[ "$terminfo[colors]" -ge 8 ]]; then
   colors
 fi
+PR_NO_COLOR="%{$terminfo[sgr0]%}"
 for color in RED GREEN YELLOW BLUE MAGENTA CYAN WHITE BLACK; do
   eval PR_LIGHT_$color='%{$terminfo[bold]$fg[${(L)color}]%}'
-  eval PR_$color='%{$fg[${(L)color}]%}'
+  eval PR_$color='%{${PR_NO_COLOR}$terminfo[normal]$fg[${(L)color}]%}'
 done
-PR_NO_COLOR="%{$terminfo[sgr0]%}"
 
 # Enable substitutions in the prompt
 setopt PROMPT_SUBST
 
 # Actual prompt variables
-PROMPT="\${PR_LIGHT_BLACK}[\${PR_NO_COLOR}\${PR_RED}%n@%m \${PR_GREEN}%2c\${PR_LIGHT_BLACK}]\${SCM_BRANCH}\${PR_NO_COLOR}%(!.#.\$) "
+PROMPT="\${PR_LIGHT_BLACK}[\${PR_RED}%n@%m\${PR_LIGHT_YELLOW}:\${PR_GREEN}%2c\${PR_LIGHT_BLACK}]\${SCM_BRANCH}\${PR_NO_COLOR}%(!.#.\$) "
 RPROMPT="\${PR_LIGHT_BLACK}[%?]\${PR_NO_COLOR}"
 
