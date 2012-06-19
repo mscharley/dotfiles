9c1ff9363e7c77ecbdfe9697e6fd03d0c561d51d
Matthew Scharley <matt.scharley@gmail.com>
2012-06-19 16:23:31 +1000

    Add a basic drushrc

---
+++ drushrc.php
@@ -0,0 +1,10 @@
+<?php
+
+// Example file available at: http://drush.ws/examples/example.drushrc.php
+
+// Load a drushrc.php configuration file from the current directory.
+$options['config'][] = '.';
+
+// Load more modules from the current directory as well.
+$options['include'] = array('./drush');
+
