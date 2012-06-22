cfd427a4f13ca2ba722f5d559f21d40f60cc92c2
Matthew Scharley <matt.scharley@gmail.com>
2012-06-22 12:14:34 +1000

    Add git workdir to the list of locations to look for drushrc.php

--- drushrc.php
+++ drushrc.php
@@ -2,7 +2,8 @@
 
 // Example file available at: http://drush.ws/examples/example.drushrc.php
 
-// Load a drushrc.php configuration file from the current directory.
+// Load a drushrc.php configuration file from the following locations.
+$options['config'][] = rtrim(`git workdir`);
 $options['config'][] = '.';
 
 // Load more modules from the current directory as well.
