b655a01b4fba135ccdac330f702b34e2e2ad815d
Matthew Scharley <matt.scharley@gmail.com>
2012-06-22 12:26:23 +1000

    Fix `git workdir`

--- drushrc.php
+++ drushrc.php
@@ -3,7 +3,8 @@
 // Example file available at: http://drush.ws/examples/example.drushrc.php
 
 // Load a drushrc.php configuration file from the following locations.
-$options['config'][] = rtrim(`git workdir`);
+$git_workdir = rtrim(`git workdir 2> /dev/null`);
+if (!empty($git_workdir)) { $options['config'][] = $git_workdir; }
 $options['config'][] = '.';
 
 // Load more modules from the current directory as well.
