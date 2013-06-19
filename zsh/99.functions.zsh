362467c0b485b40362cb58211a85a6bad6c42889
Matthew Scharley <matt.scharley@gmail.com>
2013-06-19 16:43:39 +1000

    Add a workaround for using system vagrant with rvm

--- zsh/99.functions.zsh
+++ zsh/99.functions.zsh
@@ -1,19 +1,23 @@
 #!/bin/zsh
 
 function parse_git_branch() {
-  BRANCH=$(command git branch -a 2> /dev/null | grep "^* " | sed -e 's/^\* //')
+  local BRANCH=$(command git branch -a 2> /dev/null | grep "^* " | sed -e 's/^\* //')
 
   if [[ -n "$BRANCH" ]]; then
     if [[ "$BRANCH" == "(no branch)" ]]; then
       BRANCH=$(command git name-rev HEAD 2> /dev/null | sed 's#HEAD \(.*\)#*\1#')
-    else
-      # Don't display organisation folders
-      BRANCH=$(basename $BRANCH)
     fi
 
-    WORKDIR=$(basename "$(command git workdir)")
+    local WORKDIR=$(basename "$(command git workdir)")
 
     echo " ${PR_LIGHT_BLACK}[${PR_RED}${WORKDIR}:${BRANCH}${PR_LIGHT_BLACK}]${PR_NO_COLOR}"
   fi
+}
+
+function vagrant() {
+  local curr_rvm=$rvm_env_string
+  rvm use system
+  command vagrant "$@"
+  rvm use $curr_rvm
 }
 
