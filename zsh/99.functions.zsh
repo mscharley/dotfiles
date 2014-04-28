f90f305987348ae411be8a17a41747e0e01cb354
Matthew Scharley <matt.scharley@gmail.com>
2014-04-28 17:29:48 +1000

    Add a domain-info function and refactor ZSH functions to files

--- zsh/99.functions.zsh
+++ zsh/99.functions.zsh
@@ -1,16 +1,7 @@
 #!/bin/zsh
 
-function parse_git_branch() {
-  local BRANCH=$(command git branch -a 2> /dev/null | grep "^* " | sed -e 's/^\* //')
-
-  if [[ -n "$BRANCH" ]]; then
-    if [[ "$BRANCH" == "(no branch)" ]]; then
-      BRANCH=$(command git name-rev HEAD 2> /dev/null | sed 's#HEAD \(.*\)#*\1#')
-    fi
-
-    local WORKDIR=$(basename "$(command git workdir)")
-
-    echo "${WORKDIR}:${BRANCH}"
-  fi
-}
+fpath=( ~/.zsh/functions "${fpath[@]}" )
+for f in ~/.zsh/functions/*; do
+  autoload -Uz $(basename $f)
+done
 
