0e2584e2f6b5dc63c4939c51e1d29218bf8918ec
Matthew Scharley <matt.scharley@gmail.com>
2014-07-22 12:10:38 +1000

    Add some extra fpath's for Darwin

--- zsh/4.darwin.zsh
+++ zsh/4.darwin.zsh
@@ -4,6 +4,10 @@ if [[ `uname` != "Darwin" ]]; then
   return
 fi
 
+if [[ -e /usr/local/share/zsh-completions ]]; then
+  fpath=(/usr/local/share/zsh-completions $fpath)
+fi
+
 alias ls="ls -GF"
 prepend-path /usr/local/bin
 prepend-path /usr/sbin
