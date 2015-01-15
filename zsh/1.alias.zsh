510ffd0d5bcb1459900ac702b2c9d2f2eed4080f
Matthew Scharley <matt.scharley@gmail.com>
2015-01-15 21:19:40 +1100

    Remove sass aliases

--- zsh/1.alias.zsh
+++ zsh/1.alias.zsh
@@ -7,10 +7,6 @@ alias la="ls -A"
 
 alias mem="free -m"
 
-alias sass="sass -t expanded --no-cache --unix-newlines -l --debug-info -E utf-8"
-alias sassw="sass --watch sass:css"
-alias touchsass="find ./ -iname \"*.scss\" -exec touch \{\} \;"
-
 alias php-xdebug="php -dxdebug.remote_autostart=On"
 
 alias please='sudo $(fc -ln -1)'
