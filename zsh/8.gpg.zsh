c0bb428c015464e24f7516c1c6215c21b758a198
Matthew Scharley <matt.scharley@gmail.com>
2016-05-24 13:24:43 +1000

    Add GPG agent support

---
+++ zsh/8.gpg.zsh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+
+function reload-gpg-agent() {
+  if [ -e "$HOME/.gpg-agent-info" ]; then
+    source $HOME/.gpg-agent-info
+    export GPG_AGENT_INFO
+  fi
+
+  gpg-connect-agent /bye &> /dev/null
+  if [ $? != 0 ]; then
+    eval $(gpg-agent --daemon)
+  fi
+}
+
+reload-gpg-agent
