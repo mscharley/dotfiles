b3975a9c84ff7128af92a009171bb1d94af759fa
Matthew Scharley <matt.scharley@gmail.com>
2017-03-07 23:52:25 +1100

    Don't brew so much

--- zsh/2.nvm.zsh
+++ zsh/2.nvm.zsh
@@ -2,5 +2,7 @@
 
 # TODO: Slow, lazy load.
 export NVM_DIR="$HOME/.nvm"
+if which brew &> /dev/null; then
+  NVM_DIR=$(brew --prefix nvm 2> /dev/null)
+fi
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-[ -n "$(brew --prefix nvm 2> /dev/null)" ] && . "$(brew --prefix nvm)/nvm.sh"
