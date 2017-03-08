cbd9e16f3ac3b0560bde86736a68a0094dfe6d77
Matthew Scharley <matt.scharley@gmail.com>
2017-03-08 22:20:59 +1100

    Lazy load some stuff

--- zsh/20.nvm.zsh
+++ zsh/20.nvm.zsh
@@ -1,9 +1,13 @@
 #!/usr/bin/env zsh
 
-# TODO: Slow, lazy load.
+function nvm {
+  unset -f nvm
 
-export NVM_DIR="$HOME/.nvm"
-# User-specific installation.
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-# Homebrew installation.
-[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"
+  export NVM_DIR="$HOME/.nvm"
+  # User-specific installation.
+  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+  # Homebrew installation.
+  [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"
+
+  nvm "$@"
+}
--- zsh/20.phpbrew.zsh
+++ zsh/20.phpbrew.zsh
@@ -1,14 +1,17 @@
 #!/usr/bin/env zsh
 
-# TODO: ~300ms for something I rarely use - lazy load.
 if [[ (! -d $HOME/.phpbrew) ]]; then
   function get-phpbrew {
     mkdir -p ~/bin
-    wget -qO ~/bin/phpbrew https://github.com/phpbrew/phpbrew/raw/master/phpbrew || { echo "Unable to download phpbrew successfully (requires wget)."; return 1; }
+    curl -sSL -o ~/bin/phpbrew https://github.com/phpbrew/phpbrew/raw/master/phpbrew || { echo "Unable to download phpbrew successfully (requires curl)."; return 1; }
     chmod a+x ~/bin/phpbrew
     ~/bin/phpbrew init
   }
 else
-  source $HOME/.phpbrew/bashrc
-  eval `phpbrew zsh`
+  function phpbrew {
+    unset -f phpbrew
+    source $HOME/.phpbrew/bashrc
+    eval `phpbrew zsh`
+    phpbrew "$@"
+  }
 fi
