4e571196bc55bbdca298acd7041896c931dfd77a
Matthew Scharley <matt.scharley@gmail.com>
2017-03-16 16:36:39 +1100

    Use log instead of the full diff

--- zsh/15.updates.zsh
+++ zsh/15.updates.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-local UPDATES=$(git diff --name-only origin/master..HEAD | wc -l)
+local UPDATES=$(git log --oneline --no-patch origin/master..HEAD | wc -l)
 
 if [[ $UPDATES -gt 0 ]]; then
   echo $PR_YELLOW"Updates for dotfiles detected."$PR_NO_COLOR
