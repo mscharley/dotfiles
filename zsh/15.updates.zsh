13d5a5c9808af5c3e6967d09e2436f46a4700860
Matthew Scharley <matt.scharley@gmail.com>
2017-03-19 15:00:43 +1100

    Fix the updates check

--- zsh/15.updates.zsh
+++ zsh/15.updates.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 local git_opts=(--git-dir "$(realpath ${0:a:h}/..)/.git")
-local UPDATES=$(git $git_opts log --oneline --no-patch origin/master..HEAD | wc -l)
+local UPDATES=$(git $git_opts log --oneline --no-patch origin/master...HEAD | wc -l)
 
 if [[ $UPDATES -gt 0 ]]; then
   echo "${fg[yellow]}Updates for dotfiles detected.${terminfo[sgr0]}"
