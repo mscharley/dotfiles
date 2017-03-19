2e9c3b6012d800396bee19e49a3cf5fc749e2a39
Matthew Scharley <matt.scharley@gmail.com>
2017-03-19 13:23:07 +1100

    Fix gitdir lookup during update check

--- zsh/15.updates.zsh
+++ zsh/15.updates.zsh
@@ -1,10 +1,11 @@
 #!/usr/bin/env zsh
 
-local UPDATES=$(git log --oneline --no-patch origin/master..HEAD | wc -l)
+local git_opts=(--git-dir "$(realpath ${0:a:h}/..)/.git")
+local UPDATES=$(git $git_opts log --oneline --no-patch origin/master..HEAD | wc -l)
 
 if [[ $UPDATES -gt 0 ]]; then
   echo "${fg[yellow]}Updates for dotfiles detected.${terminfo[sgr0]}"
   echo
 else
-  git --git-dir "$(realpath ${0:a:h}/..)/.git" fetch origin &> /dev/null &!
+  git $git_opts fetch origin &> /dev/null &!
 fi
