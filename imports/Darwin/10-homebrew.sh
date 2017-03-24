7aff1ccffc89dfd6f57a1f0d78021ad923e4ca30
Matthew Scharley <matt.scharley@gmail.com>
2017-03-24 21:46:03 +1100

    Add homebrew support to Darwin imports

---
+++ imports/Darwin/10-homebrew.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if which brew &> /dev/null; then true
+else
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+brew tap homebrew/bundle
+brew bundle --file=imports/Darwin/Brewfile --no-upgrade
