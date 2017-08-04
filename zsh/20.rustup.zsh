7ef2d20d7406a6cbe07129598ae8141fa39067c6
Matthew Scharley <matt.scharley@gmail.com>
2017-08-05 07:19:59 +1000

    Fix paths so that local rust is first

--- zsh/20.rustup.zsh
+++ zsh/20.rustup.zsh
@@ -1,11 +1,11 @@
 #!/usr/bin/env/zsh
 
 if [[ -d ~/.cargo/bin ]]; then
-  append-path ~/.cargo/bin
+  prepend-path ~/.cargo/bin
 else
   function rustup {
     curl https://sh.rustup.rs -sSf | sh -s -- -y --no-modify-path
-    append-path ~/.cargo/bin
+    prepend-path ~/.cargo/bin
     unset -f rustup
     rustup "$@"
   }
