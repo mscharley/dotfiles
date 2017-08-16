4f6b5d1441e7b421b8f505592420a2e3524bf4c2
Matthew Scharley <matt.scharley@gmail.com>
2017-08-16 20:59:05 +1000

    Remove bad stuff

--- imports/Darwin/30-ssh-agent-via-gpg.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Disable the OS X ssh-agent.
-sudo launchctl disable /System/Library/LaunchAgents/org.openbsd.ssh-agent.plist
-sudo launchctl unload /System/Library/LaunchAgents/org.openbsd.ssh-agent.plist
