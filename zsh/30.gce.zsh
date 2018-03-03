d0e1a6a4b495e70c4b84a2cb234aa22259faf9ad
Matthew Scharley <matt@scharley.me>
2018-03-03 17:41:30 +1100

    Make the gcloud setup a little more generic

--- zsh/30.gce.zsh
+++ zsh/30.gce.zsh
@@ -1,6 +1,8 @@
 #!/usr/bin/env zsh
 
-if [[ -d $HOME/opt/google-cloud-sdk ]]; then
-  source $HOME/opt/google-cloud-sdk/path.zsh.inc
-  source $HOME/opt/google-cloud-sdk/completion.zsh.inc
-fi
+for f in $HOME/opt/google-cloud-sdk /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk; do
+  if [[ -d "$f" ]]; then
+    source $f/path.zsh.inc
+    source $f/completion.zsh.inc
+  fi
+done
