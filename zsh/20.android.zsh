20b17773e7159a4b05d3d2ae8e128e712d3e3531
Matthew Scharley <matt@scharley.me>
2018-08-09 22:50:31 +1000

    Fix new Android SDK path on OS X

--- zsh/20.android.zsh
+++ zsh/20.android.zsh
@@ -4,4 +4,8 @@ if [[ -d /usr/local/share/android-sdk ]]; then
   export ANDROID_SDK_ROOT=/usr/local/share/android-sdk
 fi
 
-export ANDROID_HOME="$HOME/.android"
+if [[ -d $HOME/Library/Android/sdk ]]; then
+  export ANDROID_HOME="$HOME/Library/Android/sdk"
+else
+  export ANDROID_HOME="$HOME/.android"
+fi
