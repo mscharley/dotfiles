8a95e04e20aae5ff000886c6333503e5307747b3
Matthew Scharley <matt@scharley.me>
2019-06-22 00:06:12 +1000

    fix: add a nix-gc alias

--- zsh/70.nix.zsh
+++ zsh/70.nix.zsh
@@ -2,6 +2,7 @@
 
 if [[ -e ~/.nix-profile ]]; then
   source ~/.nix-profile/etc/profile.d/nix.sh
+  alias nix-gc="nix-collect-garbage --delete-older-than 14d"
 else
   function get-nix {
     NIX_INSTALLER_NO_MODIFY_PROFILE=true bash <(curl https://nixos.org/nix/install)
