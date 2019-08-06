d6d1ff960b6364e73198e746ca7500f3367e44cc
Matthew Scharley <matt.scharley@gmail.com>
2019-08-06 12:33:59 +1000

    chore: remote alacritty update reminders.
    I've deprecated my own implementation of the homebrew package and so
    this isn't necessary for me now.

--- zsh/85.updates.zsh
+++ zsh/85.updates.zsh
@@ -13,14 +13,3 @@ if [[ $UPDATES -gt 0 ]]; then
 else
   git $git_opts fetch origin &> /dev/null &!
 fi
-
-if [[ -e /Applications/Alacritty.app ]]; then
-  # Update our cached version once a day.
-  if [[ ! -e ~/.alacritty-latest-version || "$(find ~/.alacritty-latest-version -mmin +1440)" ]]; then
-    curl -s https://api.github.com/repos/jwilm/alacritty/tags | jq -r '.[].name' | head -n 1 | cut -c 2- > ~/.alacritty-latest-version
-  fi
-  if [[ -e ~/.alacritty-latest-version ]]; then
-    /Applications/Alacritty.app/Contents/MacOS/alacritty --version | grep "$(cat ~/.alacritty-latest-version)$" &> /dev/null || \
-      echo "${fg[yellow]}Updates for Alacritty detected.${terminfo[sgr0]}"$'\n'
-  fi
-fi
