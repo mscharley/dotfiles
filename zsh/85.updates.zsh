104d513354c4b4186d4574b35f53ec663a537412
Matthew Scharley <matt.scharley@gmail.com>
2019-08-29 16:52:23 +1000

    fix: a few issues with the dev command

--- zsh/85.updates.zsh
+++ zsh/85.updates.zsh
@@ -4,7 +4,12 @@ if parent-ide > /dev/null; then
   return
 fi
 
-local git_opts=(--git-dir "$(realpath ${0:a:h}/..)/.git")
+local dotfiles_dir="$(realpath ${0:a:h}/..)/.git"
+if [[ ! -e "$dotfiles_dir" ]]; then
+  return
+fi
+
+local git_opts=(--git-dir "$dotfiles_dir")
 local UPDATES=$(git $git_opts log --oneline --no-patch origin/master...HEAD | wc -l)
 
 if [[ $UPDATES -gt 0 ]]; then
