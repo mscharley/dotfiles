239f3f07521f0a9610ced2a23741bda1f855e4f8
Matthew Scharley <matt.scharley@gmail.com>
2019-10-31 14:18:23 +1100

    fix: cleanup nvm init

--- zsh/20.nvm.zsh
+++ zsh/20.nvm.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
+NVM_DIR="${XDG_CONFIG_HOME:-$HOME/opt}/nvm"
 if parent-ide > /dev/null; then
-  NVM_DIR="$HOME/.nvm"
   # User-specific installation.
   [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
   # Homebrew installation.
@@ -10,11 +10,20 @@ else
   function nvm {
     unset -f nvm
 
-    NVM_DIR="$HOME/.nvm"
     # User-specific installation.
     [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
     # Homebrew installation.
     [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh" --no-use
+
+    if which nvm &> /dev/null; then
+    else
+      echo "Installing NVM..."
+      git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
+      pushd "$NVM_DIR" > /dev/null
+      git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+      . "./nvm.sh" --no-use
+      popd > /dev/null
+    fi
 
     nvm "$@"
   }
