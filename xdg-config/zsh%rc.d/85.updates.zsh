b9194565a0a4cd98a3fb5a16c415706d14e84ff9
Matthew Scharley <matt@scharley.me>
2019-11-03 14:39:56 +1100

    fix: unset some dangling variables

--- xdg-config/zsh%rc.d/85.updates.zsh
+++ xdg-config/zsh%rc.d/85.updates.zsh
@@ -4,13 +4,13 @@ if parent-ide > /dev/null; then
   return
 fi
 
-local dotfiles_dir="$(realpath ${0:a:h}/..)/.git"
+dotfiles_dir="$(realpath ${0:a:h}/../..)/.git"
 if [[ ! -e "$dotfiles_dir" ]]; then
   return
 fi
 
-local git_opts=(--git-dir "$dotfiles_dir")
-local UPDATES=$(git $git_opts log --oneline --no-patch origin/master...HEAD | wc -l)
+git_opts=(--git-dir "$dotfiles_dir")
+UPDATES=$(git $git_opts log --oneline --no-patch origin/master...HEAD | wc -l)
 
 if [[ $UPDATES -gt 0 ]]; then
   echo "${fg[yellow]}Updates for dotfiles detected.${terminfo[sgr0]}"
@@ -18,3 +18,7 @@ if [[ $UPDATES -gt 0 ]]; then
 else
   git $git_opts fetch origin &> /dev/null &!
 fi
+
+unset dotfiles_dir
+unset git_opts
+unset UPDATES
