1f6ec096e728d62631f8b3bb3231515f145ebc3e
Matthew Scharley <matt@scharley.me>
2019-11-05 10:32:36 +1100

    fix: don't require ssh key to check for updates

--- xdg-config/zsh%rc.d/85.updates.zsh
+++ xdg-config/zsh%rc.d/85.updates.zsh
@@ -10,13 +10,19 @@ if [[ ! -e "$dotfiles_dir" ]]; then
 fi
 
 git_opts=(--git-dir "$dotfiles_dir")
-UPDATES=$(git $git_opts log --oneline --no-patch origin/master...HEAD | wc -l)
 
-if [[ $UPDATES -gt 0 ]]; then
-  echo "${fg[yellow]}Updates for dotfiles detected.${terminfo[sgr0]}"
-  echo
+if [[ -n $(git $git_opts remote | grep updates) ]]; then
+  UPDATES=$(git $git_opts log --oneline --no-patch updates/master...HEAD | wc -l)
+
+  if [[ $UPDATES -gt 0 ]]; then
+    echo "${fg[yellow]}Updates for dotfiles detected.${terminfo[sgr0]}"
+    echo
+  else
+    git $git_opts fetch updates &> /dev/null &!
+  fi
 else
-  git $git_opts fetch origin &> /dev/null &!
+  git $git_opts remote add updates https://github.com/mscharley/dotfiles.git
+  git $git_opts fetch updates &> /dev/null &!
 fi
 
 unset dotfiles_dir
