39b011839a4def2fcb5ffcfa7fd171159ae5774b
Matthew Scharley <matt.scharley@gmail.com>
2019-11-06 10:13:58 +1100

    fix: cleanup perl configuration

--- xdg-config/zsh%rc.d/05.xdg.zsh
+++ xdg-config/zsh%rc.d/05.xdg.zsh
@@ -1,6 +1,12 @@
 #!/usr/bin/env zsh
 
-# Configure XDG defaults are configured in ~/.profile
+# XDG defaults are configured in ~/.profile - ensure they were definitely set since a user may remove source
+# line for ~/.profile from their zshenv and not include new values.
+
+XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
+XDG_BIN_HOME="${XDG_BIN_HOME:-$HOME/.local/bin}"
+XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
 
 # Backport XDG into applications that can support it implicitly through configuration.
 
@@ -10,19 +16,37 @@ else
   ANDROID_HOME="${XDG_CONFIG_HOME:-$HOME/opt}/android"
 fi
 
-CARGO_HOME="${XDG_DATA_HOME}/cargo"
 DOCKER_CONFIG="${XDG_CONFIG_HOME}/docker"
 ELINKS_CONFDIR="${XDG_CONFIG_HOME}/elinks"
 ICEAUTHORITY="${XDG_CACHE_HOME}/ICEauthority"
-RUSTUP_HOME="${XDG_DATA_HOME}/rustup"
 SCREENRC="${XDG_CONFIG_HOME}/screen/screenrc"
 alias tmux='tmux -f "${XDG_CONFIG_HOME}/tmux/tmux.conf"'
 
+####
+# Node
+##
 NODE_REPL_HISTORY="${XDG_DATA_HOME}/node/repl_history"
 NPM_CONFIG_USERCONFIG="${XDG_CONFIG_HOME}/npm/npmrc"
 NVM_HOME="${NVM_DIR:-${XDG_DATA_HOME}/nvm}"
 NVM_DIR="${NVM_DIR:-${XDG_CACHE_HOME}/nvm}"
 
+####
+# Perl
+##
+append-path "$XDG_DATA_HOME/perl5/bin"
+
+PERL5LIB="$XDG_DATA_HOME/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}"
+PERL_LOCAL_LIB_ROOT="$XDG_DATA_HOME/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}"
+PERL_MB_OPT="--install_base \"$XDG_DATA_HOME/perl5\""
+PERL_MM_OPT="INSTALL_BASE=$XDG_DATA_HOME/perl5"
+
+####
+# Rust
+##
+CARGO_HOME="${XDG_DATA_HOME}/cargo"
+RUSTUP_HOME="${XDG_DATA_HOME}/rustup"
+
+# XDG Runtime support - must be made available by the OS but use it if provided.
 if [[ -n "$XDG_RUNTIME_DIR" ]]; then
   TMUX_TMPDIR="$XDG_RUNTIME_DIR"
   XAUTHORITY="${XDG_RUNTIME_DIR}/Xauthority"
--- xdg-config/zsh%rc.d/20.perl.zsh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env zsh
-
-append-path "$HOME/perl5/bin"
-
-PERL5LIB="$HOME/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}"
-PERL_LOCAL_LIB_ROOT="$HOME/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}"
-PERL_MB_OPT="--install_base \"$HOME/perl5\""
-PERL_MM_OPT="INSTALL_BASE=$HOME/perl5"
