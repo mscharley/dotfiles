9657173a09d3346927485911ac05a202c9d2a9cf
Matthew Scharley <matt@scharley.me>
2021-06-07 22:19:22 +1000

    fix: smarter xz default configuration

--- xdg-config/zsh%rc.d/01.init.zsh
+++ xdg-config/zsh%rc.d/01.init.zsh
@@ -49,3 +49,6 @@ BASH_ENV="$HOME/.bashenv"
 
 # Set a default CODE_HOME directory in case one hasn't been configured in zshenv
 CODE_HOME=${CODE_HOME:-$HOME/Code}
+
+# Configure xz compression to smartly use available processing capacity
+XZ_DEFAULTS="-T 0"
