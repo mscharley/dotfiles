f303cf33c9058d9813446459c10221a117649f78
Matthew Scharley <matt@scharley.me>
2021-09-06 09:52:12 +1000

    fix(zsh): get updates to dotfiles from the main branch

--- xdg-config/zsh%rc.d/85.updates.zsh
+++ xdg-config/zsh%rc.d/85.updates.zsh
@@ -12,7 +12,7 @@ fi
 git_opts=(--git-dir "$dotfiles_dir")
 
 if [[ -n $(git $git_opts remote | grep updates) ]]; then
-  UPDATES=$(git $git_opts log --oneline --no-patch HEAD..updates/master)
+  UPDATES=$(git $git_opts log --oneline --no-patch HEAD..updates/main)
 
   if [[ $(echo -n $UPDATES | wc -l) -gt 0 ]]; then
     echo "${fg[yellow]}Updates for dotfiles detected."
