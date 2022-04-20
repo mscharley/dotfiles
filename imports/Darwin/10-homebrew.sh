6a4f0b00c2cd055fd97ab5ebeebffe65cb27b993
Matthew Scharley <matt@scharley.me>
2022-04-20 18:03:34 +1000

    fix: updates for 2022 distribution mechanisms

--- imports/Darwin/10-homebrew.sh
+++ imports/Darwin/10-homebrew.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if ! command -v brew &> /dev/null; then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 brew tap homebrew/bundle
