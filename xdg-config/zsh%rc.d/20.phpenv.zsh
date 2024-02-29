c2649d1ac34341cbc8c716c77bba688ae011b2da
Matthew Scharley <matt@scharley.me>
2024-02-29 21:30:17 +1100

    fix: update phpenv setup

--- xdg-config/zsh%rc.d/20.phpenv.zsh
+++ xdg-config/zsh%rc.d/20.phpenv.zsh
@@ -1,9 +1,14 @@
 #!/usr/bin/env zsh
 
-if [[ (! -d $HOME/.phpenv) ]]; then
-  alias get-phpenv="git clone git://github.com/phpenv/phpenv.git ~/.phpenv; echo You will need to reload your shell now."
+export PHPENV_ROOT=$XDG_CACHE_HOME/phpenv
+if [[ (! -d $PHPENV_ROOT) ]]; then
+	function get-phpenv {
+		git clone https://github.com/phpenv/phpenv.git $PHPENV_ROOT
+		git clone https://github.com/php-build/php-build $PHPENV_ROOT/plugins/php-build
+		git clone https://github.com/ngyuki/phpenv-composer.git $PHPENV_ROOT/plugins/phpenv-composer
+		echo You will need to reload your shell now.
+	}
 else
-  prepend-path "$HOME/.phpenv/bin"
-  prepend-path "$HOME/.phpenv/shims"
-  source "$HOME/.phpenv/completions/phpenv.zsh"
+	prepend-path "$PHPENV_ROOT/bin"
+	eval "$(phpenv init -)"
 fi
