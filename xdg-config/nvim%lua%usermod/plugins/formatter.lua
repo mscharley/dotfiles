29a72a5bd577dada798d384763643ad1dcca1cdf
Matthew Scharley <matt@scharley.me>
2024-10-10 18:27:32 +1100

    fix(nvim): only use prettier if there's a config for it

--- xdg-config/nvim%lua%usermod/plugins/formatter.lua
+++ xdg-config/nvim%lua%usermod/plugins/formatter.lua
@@ -1,12 +1,16 @@
 require('usermod.vimplug')('mhartington/formatter.nvim', {
 	config = function()
+		local formatters = {}
+		local prettierrc = vim.fs.find('.prettierrc', { path = "./", type = "file", upward = true })
+		if #prettierrc > 0 then
+			table.insert(formatters, require('formatter.defaults.prettier'))
+		end
+
 		require('formatter').setup({
 			logging = true,
 			log_level = vim.log.levels.WARN,
 			filetype = {
-				['*'] = {
-					require('formatter.defaults.prettier'),
-				},
+				['*'] = formatters,
 			},
 		})
 		vim.api.nvim_create_augroup("__formatter__", { clear = true })
