f749b3db48637b885f9635f91e9ab412afa47ffd
Matthew Scharley <matt@scharley.me>
2024-10-30 14:43:42 +1100

    fix(nvim): cleanup some weird edge cases

--- configs/xdg/config/nvim%lua%usermod/plugins/fzf.lua
+++ configs/xdg/config/nvim%lua%usermod/plugins/fzf.lua
@@ -1,12 +1,12 @@
--- Binary dependencies: fzf, bat, fd
+-- Binary dependencies: fzf, bat, fd, ripgrep
 return {
 	{
 		'ibhagwan/fzf-lua',
 		opts = { 'fzf-native' },
 		keys = {
-			{ '<leader>g', function(...) require('fzf-lua').files(unpack(arg)) end, desc = "Fuzzy find files" },
-			{ '<leader>r', function(...) require('fzf-lua').live_grep_resume(unpack(arg)) end, desc = "Search project" },
-			{ '<leader>R', function(...) require('fzf-lua').live_grep(unpack(arg)) end, desc = "Search project (clean)" },
+			{ '<leader>g', function() require('fzf-lua').files({}) end, desc = "Fuzzy find files" },
+			{ '<leader>r', function() require('fzf-lua').live_grep_resume({}) end, desc = "Search project" },
+			{ '<leader>R', function() require('fzf-lua').live_grep({}) end, desc = "Search project (clean)" },
 		},
 	},
 }
