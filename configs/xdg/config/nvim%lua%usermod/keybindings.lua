58d0c5926ced97db954bf881a2bdf1598a16869a
Matthew Scharley <matt@scharley.me>
2024-10-31 18:14:13 +1100

    chore: lint

--- configs/xdg/config/nvim%lua%usermod/keybindings.lua
+++ configs/xdg/config/nvim%lua%usermod/keybindings.lua
@@ -1,28 +1,28 @@
 -- Navigate by visual line instead of logical line
-vim.keymap.set("", "<down>", "gj")
-vim.keymap.set("", "<up>", "gk")
+vim.keymap.set({}, '<down>', 'gj')
+vim.keymap.set({}, '<up>', 'gk')
 
 -- Move line up/down
-vim.keymap.set("n", "<A-up>", "dd<up>P", { desc = "Move line up" })
-vim.keymap.set("n", "<A-down>", "ddp", { desc = "Move line down" })
+vim.keymap.set({'n'}, '<A-up>', 'dd<up>P', { desc = "Move line up" })
+vim.keymap.set({'n'}, '<A-down>', 'ddp', { desc = "Move line down" })
 
 -- Navigate between split buffers
-vim.keymap.set("n", "<C-A-up>", "<C-w><up>")
-vim.keymap.set("n", "<C-A-down>", "<C-w><down>")
-vim.keymap.set("n", "<C-A-left>", "<C-w><left>")
-vim.keymap.set("n", "<C-A-right>", "<C-w><right>")
+vim.keymap.set({'n'}, '<C-A-up>', '<C-w><up>')
+vim.keymap.set({'n'}, '<C-A-down>', '<C-w><down>')
+vim.keymap.set({'n'}, '<C-A-left>', '<C-w><left>')
+vim.keymap.set({'n'}, '<C-A-right>', '<C-w><right>')
 
 -- Split buffers
-vim.keymap.set("n", "<leader>\"", "<Cmd>vsplit<CR>", { desc = "Split screen (vertical)" })
-vim.keymap.set("n", "<leader>%", "<Cmd>split<CR>", { desc = "Split screen (horizontal)" })
+vim.keymap.set({'n'}, '<leader>"', "<Cmd>vsplit<CR>", { desc = 'Split screen (vertical)' })
+vim.keymap.set({'n'}, '<leader>%', "<Cmd>split<CR>", { desc = 'Split screen (horizontal)' })
 
 -- Move to previous/next tab
-vim.keymap.set('n', '<A-,>', '<Cmd>BufferPrevious<CR>', { desc = "View buffer left" })
-vim.keymap.set('n', '<A-.>', '<Cmd>BufferNext<CR>', { desc = "View buffer right" })
+vim.keymap.set({'n'}, '<A-,>', '<Cmd>BufferPrevious<CR>', { desc = 'View buffer left' })
+vim.keymap.set({'n'}, '<A-.>', '<Cmd>BufferNext<CR>', { desc = 'View buffer right' })
 
 -- Re-order to previous/next tab
-vim.keymap.set('n', '<D-A-,>', '<Cmd>BufferMovePrevious<CR>', { desc = "Move buffer left" })
-vim.keymap.set('n', '<D-A-.>', '<Cmd>BufferMoveNext<CR>', { desc = "Move buffer right" })
+vim.keymap.set({'n'}, '<D-A-,>', '<Cmd>BufferMovePrevious<CR>', { desc = 'Move buffer left' })
+vim.keymap.set({'n'}, '<D-A-.>', '<Cmd>BufferMoveNext<CR>', { desc = 'Move buffer right' })
 
 -- -- Goto buffer in position...
 -- vim.keymap.set('n', '<A-1>', '<Cmd>BufferGoto 1<CR>')
@@ -40,7 +40,7 @@ vim.keymap.set('n', '<D-A-.>', '<Cmd>BufferMoveNext<CR>', { desc = "Move buffer 
 -- vim.keymap.set('n', '<A-p>', '<Cmd>BufferPin<CR>')
 
 -- Close buffer
-vim.keymap.set('n', '<A-q>', '<Cmd>BufferClose<CR>', { desc = "Close buffer" })
+vim.keymap.set({'n'}, '<A-q>', '<Cmd>BufferClose<CR>', { desc = 'Close buffer' })
 
 -- Wipeout buffer
 --                 :BufferWipeout
@@ -56,8 +56,8 @@ vim.keymap.set('n', '<A-q>', '<Cmd>BufferClose<CR>', { desc = "Close buffer" })
 -- map('n', '<C-p>', '<Cmd>BufferPick<CR>')
 
 -- Sort automatically by...
--- vim.keymap.set('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>')
--- vim.keymap.set('n', '<Space>bn', '<Cmd>BufferOrderByName<CR>')
--- vim.keymap.set('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>')
--- vim.keymap.set('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>')
--- vim.keymap.set('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>')
+-- vim.keymap.set('n', '<Leader>bb', '<Cmd>BufferOrderByBufferNumber<CR>')
+-- vim.keymap.set('n', '<Leader>bn', '<Cmd>BufferOrderByName<CR>')
+-- vim.keymap.set('n', '<Leader>bd', '<Cmd>BufferOrderByDirectory<CR>')
+-- vim.keymap.set('n', '<Leader>bl', '<Cmd>BufferOrderByLanguage<CR>')
+-- vim.keymap.set('n', '<Leader>bw', '<Cmd>BufferOrderByWindowNumber<CR>')
