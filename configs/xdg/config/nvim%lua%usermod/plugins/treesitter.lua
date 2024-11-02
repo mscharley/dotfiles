257786da55a16400cf38a7929d67dc5e4a432c7c
Matthew Scharley <matt@scharley.me>
2024-11-03 02:06:52 +1100

    fix(nvim): add basic mdx support

--- configs/xdg/config/nvim%lua%usermod/plugins/treesitter.lua
+++ configs/xdg/config/nvim%lua%usermod/plugins/treesitter.lua
@@ -1,20 +1,28 @@
 return {
 	{
 		'nvim-treesitter/nvim-treesitter',
-		event = "VimEnter",
-		run = ":TSUpdate",
+		event = 'VimEnter',
+		run = ':TSUpdate',
 		config = function()
 			require('nvim-treesitter.configs').setup({
 				ensure_installed = {
-					"yaml", "json", "json5", "markdown", "toml",
-					"lua", "vim", "vimdoc",
-					"graphql",
-					"perl",
-					"javascript", "typescript",
-					"gleam", "rust",
+					'yaml', 'json', 'json5', 'markdown', 'toml',
+					'lua', 'vim', 'vimdoc',
+					'graphql',
+					'perl',
+					'javascript', 'typescript',
+					'gleam', 'rust',
 				},
 				highlight = { enable = true },
 			})
+
+			vim.filetype.add({
+				extension = {
+					mdx = 'mdx',
+				},
+			})
+
+			vim.treesitter.language.register('markdown', 'mdx')
 		end
 	},
 }
