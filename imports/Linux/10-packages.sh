acf2684a60571ff9e2ab1c82a7d18395d228acb4
Matthew Scharley <matt@scharley.me>
2024-11-17 21:57:30 +1100

    fix: more minor updates for new installations

--- imports/Linux/10-packages.sh
+++ imports/Linux/10-packages.sh
@@ -2,12 +2,12 @@
 
 if [ -e /usr/sbin/transactional-update ]; then
 	sudo transactional-update --continue pkg install \
-		zsh fish starship gcc most kitty \
+		zsh fish starship git gcc most kitty \
 		fira-code-fonts symbols-only-nerd-fonts google-noto-sans-cjk-fonts \
 		neovim fzf ripgrep fd bat
 elif which zypper &> /dev/null; then
 	sudo zypper install \
-		zsh fish starship gcc most kitty \
+		zsh fish starship git gcc most kitty \
 		fira-code-fonts symbols-only-nerd-fonts google-noto-sans-cjk-fonts \
 		neovim fzf ripgrep fd bat
 fi
