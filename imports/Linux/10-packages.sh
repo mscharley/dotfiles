053d001be9fb42190246de4bad45e3178c618b85
Matthew Scharley <matt@scharley.me>
2024-11-22 01:05:20 +1100

    fix(pkgs): more important linux packages not installed by default on kalpa

--- imports/Linux/10-packages.sh
+++ imports/Linux/10-packages.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
 if [ -e /usr/sbin/transactional-update ]; then
-	sudo transactional-update --continue pkg install \
+	sudo transactional-update -d --continue pkg install -t pattern devel_basis
+	sudo transactional-update -d --continue pkg install \
 		zsh fish starship git gcc most kitty \
 		fira-code-fonts symbols-only-nerd-fonts google-noto-sans-cjk-fonts \
-		neovim fzf ripgrep fd bat
+		neovim fzf ripgrep fd bat wl-clipboard \
+		libopenssl-devel
 elif which zypper &> /dev/null; then
+	sudo zypper install -t pattern devel_basis
 	sudo zypper install \
 		zsh fish starship git gcc most kitty \
 		fira-code-fonts symbols-only-nerd-fonts google-noto-sans-cjk-fonts \
-		neovim fzf ripgrep fd bat
+		neovim fzf ripgrep fd bat wl-clipboard \
+		libopenssl-devel
 fi
 
 # Reset sudo access
