417aeeb19bcd44fdf48c18a08e07e1a911ce42ef
Matthew Scharley <matt@scharley.me>
2024-12-24 22:45:08 +1100

    chore(installer): install kde connect out of the box

--- imports/Linux/10-packages.sh
+++ imports/Linux/10-packages.sh
@@ -5,14 +5,14 @@ if [ -e /usr/sbin/transactional-update ]; then
 	sudo transactional-update -d --continue pkg install \
 		zsh fish starship git gcc most kitty \
 		fira-code-fonts symbols-only-nerd-fonts google-noto-sans-cjk-fonts \
-		neovim fzf ripgrep fd bat wl-clipboard \
+		neovim fzf ripgrep fd bat wl-clipboard kdeconnect-kde \
 		libopenssl-devel
 elif which zypper &> /dev/null; then
 	sudo zypper install -t pattern devel_basis
 	sudo zypper install \
 		zsh fish starship git gcc most kitty \
 		fira-code-fonts symbols-only-nerd-fonts google-noto-sans-cjk-fonts \
-		neovim fzf ripgrep fd bat wl-clipboard \
+		neovim fzf ripgrep fd bat wl-clipboard kdeconnect-kde \
 		libopenssl-devel
 fi
 
