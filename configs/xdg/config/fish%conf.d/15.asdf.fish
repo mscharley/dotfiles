5dffeafe5960fe464e9488374e9cd974cb1302aa
Matthew Scharley <matt@scharley.me>
2025-05-18 03:14:22 +1000

    fix: bump to support asdf 0.16

--- configs/xdg/config/fish%conf.d/15.asdf.fish
+++ configs/xdg/config/fish%conf.d/15.asdf.fish
@@ -3,37 +3,24 @@ export ASDF_DIR="$XDG_DATA_HOME/asdf"
 export ASDF_DATA_DIR="$XDG_CACHE_HOME/asdf"
 export ASDF_NODEJS_AUTO_ENABLE_COREPACK="true"
 
-if test -s "$ASDF_DIR"
-	if ! test -e "$ASDF_DIR/completions/asdf.fish"
-		ln -s "$ASDF_DIR/completions/asdf.fish" "$XDG_CONFIG_HOME/fish/completions/asdf.fish"
-	end
-	source "$ASDF_DIR/asdf.fish"
-
-	# asdf shims hide the real path to where corepack installs the binaries.
-	abbr --add yarn "corepack yarn"
-	abbr --add yarnpkg "corepack yarnpkg"
-	abbr --add pnpm "corepack pnpm"
-	abbr --add pnpx "corepack pnpx"
-	abbr --add npm "corepack npm"
-	abbr --add npx "corepack npx"
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
 else
-	function init-asdf
-		git clone https://github.com/asdf-vm/asdf.git "$ASDF_DIR"
-		pushd $ASDF_DIR &> /dev/null
-		git checkout "$(git tag --sort version:refname | tail -n 1)"
-		popd &> /dev/null
-		ln -s "$ASDF_DIR/completions/asdf.fish" "$XDG_CONFIG_HOME/fish/completions/asdf.fish"
-		source "$ASDF_DIR/asdf.fish"
-
-		asdf plugin add nodejs
-		asdf plugin add ruby
-
-		# asdf shims hide the real path to where corepack installs the binaries.
-		abbr --add yarn "corepack yarn"
-		abbr --add yarnpkg "corepack yarnpkg"
-		abbr --add pnpm "corepack pnpm"
-		abbr --add pnpx "corepack pnpx"
-		abbr --add npm "corepack npm"
-		abbr --add npx "corepack npx"
-	end
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
 end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
+
+# asdf shims hide the real path to where corepack installs the binaries.
+abbr --add yarn "corepack yarn"
+abbr --add yarnpkg "corepack yarnpkg"
+abbr --add pnpm "corepack pnpm"
+abbr --add pnpx "corepack pnpx"
+abbr --add npm "corepack npm"
+abbr --add npx "corepack npx"
