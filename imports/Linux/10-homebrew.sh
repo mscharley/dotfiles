2ca1f09a5d64251a6f86e3d86346e380ccffc183
Matthew Scharley <matt@scharley.me>
2025-06-08 14:04:13 +1000

    fix: optionally use linuxbrew, but don't assume we want it

--- imports/Linux/10-homebrew.sh
+++ imports/Linux/10-homebrew.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-if ! command -v brew &> /dev/null; then
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-	eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+if command -v brew &> /dev/null; then
+	brew bundle --file=configs/xdg/config/homebrew%Brewfile --no-upgrade
 fi
-
-brew bundle --file=configs/xdg/config/homebrew%Brewfile --no-upgrade
