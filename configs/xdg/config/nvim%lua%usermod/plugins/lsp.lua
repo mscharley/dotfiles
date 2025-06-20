b7940d974d59ad3948504aa55fc9ca6154dc06ad
Matthew Scharley <matt@scharley.me>
2025-06-21 07:37:10 +1000

    fix(nvim): handle lua errors in vim with lazydev

--- configs/xdg/config/nvim%lua%usermod/plugins/lsp.lua
+++ configs/xdg/config/nvim%lua%usermod/plugins/lsp.lua
@@ -46,6 +46,35 @@ return {
 		},
 	},
 	{
+		"folke/lazydev.nvim",
+		ft = "lua", -- only load on lua files
+		opts = {
+			library = {
+				-- Library paths can be absolute
+				-- "~/projects/my-awesome-lib",
+				-- Or relative, which means they will be resolved from the plugin dir.
+				"lazy.nvim",
+				-- It can also be a table with trigger words / mods
+				-- Only load luvit types when the `vim.uv` word is found
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+				-- always load the LazyVim library
+				"LazyVim",
+				-- Only load the lazyvim library when the `LazyVim` global is found
+				{ path = "LazyVim", words = { "LazyVim" } },
+				-- Load the wezterm types when the `wezterm` module is required
+				-- Needs `justinsgithub/wezterm-types` to be installed
+				-- { path = "wezterm-types", mods = { "wezterm" } },
+				-- Load the xmake types when opening file named `xmake.lua`
+				-- Needs `LelouchHe/xmake-luals-addon` to be installed
+				-- { path = "xmake-luals-addon/library", files = { "xmake.lua" } },
+			},
+			enabled = function(root_dir)
+				-- This is slightly different to the default, where it will be enabled by default
+				return vim.g.lazydev_enabled == nil and false or vim.g.lazydev_enabled
+			end,
+		},
+	},
+	{
 		'neovim/nvim-lspconfig',
 		version = '*',
 		dependencies = { 'williamboman/mason-lspconfig.nvim' },
