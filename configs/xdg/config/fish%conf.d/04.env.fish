92ed39c1f028284ca81767eb2ee68a8b0b6aa61b
Matthew Scharley <matt@scharley.me>
2025-09-01 01:33:06 +1000

    fix: add support for direnv

--- configs/xdg/config/fish%conf.d/04.env.fish
+++ configs/xdg/config/fish%conf.d/04.env.fish
@@ -26,3 +26,7 @@ else if type dircolors &> /dev/null
 else
 	export LS_COLORS="rs=0:di=34:ln=36:so=35:pi=33:ex=32:bd=1;33;40:cd=1;33;40:su=1;32;1;41:sg=32;1;43:tw=1;34;42:ow=1;34;42"
 end
+
+if type direnv &> /dev/null
+	direnv hook fish | source
+end
