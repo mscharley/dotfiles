c48c8d2a6e914647909a2439c8f5bcab3909b2ae
Matthew Scharley <matt@scharley.me>
2025-09-05 19:19:52 +1000

    fix: remove unneeded asdf configuration for 0.16 onwards

--- configs/xdg/config/fish%conf.d/15.asdf.fish
+++ configs/xdg/config/fish%conf.d/15.asdf.fish
@@ -1,19 +1,18 @@
 export ASDF_CONFIG_FILE="$XDG_CONFIG_HOME/asdf/asdfrc"
-export ASDF_DIR="$XDG_DATA_HOME/asdf"
 export ASDF_DATA_DIR="$XDG_CACHE_HOME/asdf"
 export ASDF_NODEJS_AUTO_ENABLE_COREPACK="true"
 
 # ASDF configuration code
 if test -z $ASDF_DATA_DIR
-    set _asdf_shims "$HOME/.asdf/shims"
+	set _asdf_shims "$HOME/.asdf/shims"
 else
-    set _asdf_shims "$ASDF_DATA_DIR/shims"
+	set _asdf_shims "$ASDF_DATA_DIR/shims"
 end
 
 # Do not use fish_add_path (added in Fish 3.2) because it
 # potentially changes the order of items in PATH
 if not contains $_asdf_shims $PATH
-    set -gx --prepend PATH $_asdf_shims
+	set -gx --prepend PATH $_asdf_shims
 end
 set --erase _asdf_shims
 
