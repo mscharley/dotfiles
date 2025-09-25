51385adf68493f6f8e5cd9342b3ffad6ca799f50
Matthew Scharley <matt@scharley.me>
2025-09-25 20:34:02 +1000

    fix(fish): set abbreviations and aliases in the last file to load

--- configs/xdg/config/fish%conf.d/10.abbr.fish
+++
@@ -1,6 +0,0 @@
-if type podman &> /dev/null
-	abbr --add docker podman
-	abbr --add dc 'podman compose'
-else
-	abbr --add dc 'docker compose'
-end
--- configs/xdg/config/fish%config.fish
+++ configs/xdg/config/fish%config.fish
@@ -1,3 +1,16 @@
 if status is-interactive
 	# Commands to run in interactive sessions can go here
+	if type podman &> /dev/null
+		abbr --add docker podman
+		abbr --add dc 'podman compose'
+	else
+		abbr --add dc 'docker compose'
+	end
+
+	if type gls &> /dev/null
+		alias ls="gls --color=tty -F"
+	else
+		alias ls="ls --color=tty -F"
+	end
+	alias ll="ls -lAh"
 end
