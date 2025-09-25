d461fa2de52e1ce683f3548217e70cd5b1ee1b4a
Matthew Scharley <matthew.scharley@getequiem.com>
2025-09-25 14:47:34 +1000

    fix: enable folke/lazydev to get lua autocomplete working in this repo

--- configs/xdg/config/nvim%lua%usermod/plugins/lsp.lua
+++ configs/xdg/config/nvim%lua%usermod/plugins/lsp.lua
@@ -78,7 +78,7 @@ return {
 		'neovim/nvim-lspconfig',
 		version = '*',
 		dependencies = { 'williamboman/mason-lspconfig.nvim' },
-		event = "BufReadPost",
+		event = 'BufReadPost',
 		config = function()
 			-- Strip out some default keybindings
 			vim.keymap.del('n', 'gra')
@@ -121,46 +121,6 @@ return {
 				on_attach = function(client, bufnr)
 					vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
 				end
-			})
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-				settings = { Lua = {  } },
-				on_init = function(client)
-					-- Helper to load the VIM runtimes when editing lua files
-					if client.workspace_folders then
-						local path = client.workspace_folders[1].name
-						if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
-							return
-						end
-					end
-
-					client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-						runtime = {
-							-- Tell the language server which version of Lua you're using
-							-- (most likely LuaJIT in the case of Neovim)
-							version = 'LuaJIT'
-						},
-						diagnostics = {
-							-- Get the language server to recognise the `vim` global
-							globals = {
-								'vim',
-								'require',
-							},
-						},
-						-- Make the server aware of Neovim runtime files
-						workspace = {
-							checkThirdParty = false,
-							-- library = {
-							-- 	vim.env.VIMRUNTIME
-							-- 	-- Depending on the usage, you might want to add additional paths here.
-							-- 	-- "${3rd}/luv/library"
-							-- 	-- "${3rd}/busted/library",
-							-- }
-							-- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-							library = vim.api.nvim_get_runtime_file("", true)
-						}
-					})
-				end,
 			})
 			lspconfig.jsonls.setup({
 				capabilities = capabilities,
