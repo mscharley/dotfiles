2fad9660583a94ca0d9404640959ec94a8476782
Matthew Scharley <matt@scharley.me>
2026-06-18 12:55:08 +1000

    fix: don't trigger nix-updates warning in non-interactive fish scripts

--- configs/xdg/config/fish%conf.d/98.nix-updates.fish
+++ configs/xdg/config/fish%conf.d/98.nix-updates.fish
@@ -1,7 +1,9 @@
-if test -e $CODE_HOME/mscharley/nixos
-	# Last modified more than one week ago (in seconds)
-	if test (path mtime --relative $CODE_HOME/mscharley/nixos/flake.lock) -ge 604800
-		echo (set_color --bold brred)"Updates for NixOS available."(set_color reset)
-		echo
+if status is-interactive
+	if test -e $CODE_HOME/mscharley/nixos
+		# Last modified more than one week ago (in seconds)
+		if test (path mtime --relative $CODE_HOME/mscharley/nixos/flake.lock) -ge 604800
+			echo (set_color --bold brred)"Updates for NixOS available."(set_color reset)
+			echo
+		end
 	end
 end
